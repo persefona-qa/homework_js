9fc6354c15db470a7e1fa20d7a6f79cb48997cbe
persefona-qa <dubkonata@gmail.com>
2024-11-12 11:45:06 +0200

    task 3

--- HW3.js
+++ HW3.js
@@ -69,3 +69,84 @@ console.log(scores);
  console.log (stud3.fullName)
  console.log(stud3.nameIncludes ('Ivan'))
  console.log(stud3.nameIncludes ('Koval'))
+
+ //task 3
+
+ class Plane 
+ {
+    constructor (model, fuelSupply, fuelConsumption)
+    {
+        this.model = model
+        this.fuelSupply = fuelSupply
+        this.fuelConsumption = fuelConsumption
+    }
+
+    calcFlightRange()
+    {
+       return (this.fuelSupply/this.fuelConsumption)*100
+    }
+
+   static sortFlightRange (planesArray)
+    {
+        planesArray.sort((a,b) => a.calcFlightRange() - b.calcFlightRange())
+        console.log ("Sorted range of planes")
+        planesArray.forEach(plane => {
+            console.log(`${plane.model}:${plane.calcFlightRange()}`)
+        });
+    }
+ }
+
+ class TransportPlane extends Plane
+ {
+    constructor (model, fuelSupply, fuelConsumption, cargo, addTank)
+    {
+        super (model, fuelSupply, fuelConsumption)
+        this.cargo = cargo
+        this.addTank = addTank
+    }
+
+    calcFlightRange()
+    {
+        return ((this.fuelSupply+this.addTank)/this.fuelConsumption)*100
+    }
+ }
+
+ class PassengerPlane extends Plane
+ {
+    constructor (model, fuelSupply, fuelConsumption, passangers, refueling)
+    {
+        super (model, fuelSupply, fuelConsumption)
+    this.passangers = passangers
+    this.refueling = refueling
+    }
+    calcFlightRange ()
+    {
+        return ((this.fuelSupply+this.refueling)/this.fuelConsumption)*100
+    }
+ }
+
+ class WarPlane extends Plane
+ {
+    constructor (model, fuelSupply, fuelConsumption, missles, aerodynamicsKoef)
+    {
+        super (model, fuelSupply, fuelConsumption)
+    this.missles = missles
+    this.aerodynamicsKoef = aerodynamicsKoef
+    }
+    calcFlightRange()
+     {
+        return((this.fuelSupply/this.fuelConsumption)*100)*this.aerodynamicsKoef
+    }
+ }
+
+
+ const plane1 = new TransportPlane("An-225 Mriya", 105000, 5000, 500, 300000);
+ const plane2 = new PassengerPlane("Boeing-747", 193000, 2500, 410, 90000);
+ const plane3 = new WarPlane("F-22 Raptor", 8200, 320, 20, 1.2);
+ const planesArray = [plane1, plane2, plane3]
+
+ console.log("Unsorted range of planes:");
+ console.log(plane1.model, plane1.calcFlightRange())
+ console.log(plane2.model, plane2.calcFlightRange())
+ console.log(plane3.model, plane3.calcFlightRange())
+Plane.sortFlightRange(planesArray)
